1e77a9e6200022727a0163e38f6e098d44a9c475
namngo93 <ngonam199380@gmail.com>
2023-02-03 09:11:35 +0700

    up login

--- BE/src/controllers/userController.ts
+++ BE/src/controllers/userController.ts
@@ -25,18 +25,7 @@ class ProductController {
     }
 
 
-    // list =  async (req: Request, res: Response) => {
-    //     let id = req.params.id;
-    //     let newHome = await this.homeService.findByID(id)
-    //     // console.log(newHome)
-    //     // @ts-ignore
-    //     let user = req.session.User;
-    //     console.log()
-    //     newHome.user.username = user.username;
-    //     await this.homeService.update(id,newHome);
-    //     let homes= await homeService.findByUsername(user);
-    //     res.render('users/myHomeList', {homes: homes, user:user});
-    // }
+
 
 }
 export default new ProductController();
