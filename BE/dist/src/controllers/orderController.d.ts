de3592d7826f4bf9a70e19f9b6213dd5d2299b67
daianh <115562284+daianh@users.noreply.github.com>
2023-02-04 16:36:54 +0700

    nam dan them order BE

--- BE/dist/src/controllers/orderController.d.ts
+++ BE/dist/src/controllers/orderController.d.ts
@@ -1,13 +1,9 @@
 import { Request, Response } from "express";
-import { Request, Response } from "express";
 declare class OrderController {
     private orderService;
     constructor();
     getAll: (req: Request, res: Response) => Promise<void>;
     create: (req: Request, res: Response) => Promise<void>;
-    update: (req: Request, res: Response) => Promise<void>;
-    remove: (req: Request, res: Response) => Promise<void>;
-    getAll: (req: Request, res: Response) => Promise<void>;
     update: (req: Request, res: Response) => Promise<void>;
     remove: (req: Request, res: Response) => Promise<void>;
 }
